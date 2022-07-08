21eae202a66bd9cf059fa2f3b3c2299d56bde56a
Ryan-Pupia <ryanpupia@gmail.com>
2022-07-08 00:41:53 -0400

    Realizado Projeto de contador JS

---
+++ Anotações/JavaScript/teste.js
@@ -0,0 +1,14 @@
+// Comentario de linha
+var z = 1;
+/* Comentario de
+varias linhas*/
+z = 0
+// Variaveis podem ter seu valor alterado
+const BOLACHA = 3
+// Constantes não
+//funções são operações a serem realizadas em seus parametros
+function soma(a, b) {
+    return a + b
+}
+console.log(soma(z, BOLACHA))
+//Se pode rodar esse codigo no terminal através do node js
