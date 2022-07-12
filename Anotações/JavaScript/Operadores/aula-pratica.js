d20ee64b9df57f114f48498c72dca27fbff37c85
Ryan-Pupia <ryanpupia@gmail.com>
2022-07-12 17:05:37 -0400

    Melhorada aula pratica de operadores em JS
    Adicionada validação de parametros em aula pratica de operadores em JS
    :wq

--- Anotações/JavaScript/Operadores/aula-pratica.js
+++ Anotações/JavaScript/Operadores/aula-pratica.js
@@ -1,21 +1,25 @@
 function comparador(num1, num2) {
-    var comparação = `Os numeros ${num1} e ${num2} `;
-    const igualdade = num1 === num2;
-    const soma = num1 + num2;
-    var maior10;
-    var maior20;
-    igualdade ? comparação += "são iguais." : comparação += "não são iguais. ";
-    comparação += `Sua soma é igual a ${soma}. `
-    if (soma > 10) {
-        maior10 = ""
-        soma > 20 ? maior20 = "" : maior20 = " não";
-    }
+    if (!num1 || !num2) return "Defina dois números"
+    else if (typeof(num1) != "number" || typeof(num2) != "number") return "Os parametros devem ser números"
     else {
-        maior10 = " não";
-        maior20 = " não";
+        var comparação = `Os numeros ${num1} e ${num2} `;
+        const igualdade = num1 === num2;
+        const soma = num1 + num2;
+        var maior10;
+        var maior20;
+        igualdade ? comparação += "são iguais." : comparação += "não são iguais. ";
+        comparação += `Sua soma é igual a ${soma}. `
+        if (soma > 10) {
+            maior10 = "maior"
+            soma > 20 ? maior20 = "maior" : maior20 = "menor";
+        }
+        else {
+            maior10 = "menor";
+            maior20 = "menor";
+        }
+        comparação += `${soma} é ${maior10} que 10, e ${maior20} que 20.`;
+        return comparação;
     }
-    comparação += `${soma}${maior10} é maior que 10, e${maior20} é maior que 20.`;
-    return comparação;
 }
 
-console.log(comparador(10, 3));
+console.log(comparador(3, 3));
